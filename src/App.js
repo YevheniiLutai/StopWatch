4f411c13b45e46e674210f14583fe15821e34c6a
Zhenia <jenia940@gmail.com>
2021-06-25 01:50:17 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimerOn] = React.useState(false);
+
+  function Start() { // Start timer
+    setTimerOn(true);
+  }
+
+  function Stop() { // Stop timer
+    setTimerOn(false);
+  }
+
+  function Reset() { // Reset timer to 0
+    setTime(0);
+  }
+
+  function ResetDoubleClick() {
+    setTimerOn(false);
+  }
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!timerOn) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="stopwatch">
+      <div className="title">
+        <h2>Lutai Yevhenii</h2>
+      </div>
+
+      <div className="сlock_face">
+        <div id="display">
+          <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+          <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+      </div>
+
+      <div className="functionals">
+        <div id="buttons">
+          <button
+            onClick={() => {
+              if (!timerOn) {
+                Start();
+              } else if (time > 0) {
+                Stop();
+                Reset();
+              }
+            }}
+          >
+            Start / Stop
+          </button>
+          <button onDoubleClick={ResetDoubleClick}>Wait</button>
+          <button
+            onClick={() => {
+              Reset();
+              Start();
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
